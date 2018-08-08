f2fb5a36ef40613e7fdbc0407c7ab1e927869722
Sean Mooneyham <swmooneyham@gmail.com>
2018-08-08 15:20:39 -0400

    Refactored current landing page giving it a basic admin panel layout

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToolListComponent } from './tools/tool-list/tool-list.component';
+import { ToolEditComponent } from './tools/tool-edit/tool-edit.component';
+import { MaterialListComponent } from './material/material-list/material-list.component';
+import { MaterialEditComponent } from './material/material-edit/material-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolListComponent,
+    ToolEditComponent,
+    MaterialListComponent,
+    MaterialEditComponent
   ],
   imports: [
     BrowserModule
---
+++ client/src/app/tools/tool-edit/tool-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-edit',
+  templateUrl: './tool-edit.component.html',
+  styleUrls: ['./tool-edit.component.css']
+})
+export class ToolEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
