c664174a776eff77b3da63ec42d138b9f71d7b4a
Sean Mooneyham <swmooneyham@gmail.com>
2018-08-09 11:44:56 -0400

    Added basic routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { ToolListComponent } from './tools/tool-list/tool-list.component';
 import { ToolEditComponent } from './tools/tool-edit/tool-edit.component';
 import { MaterialListComponent } from './material/material-list/material-list.component';
 import { MaterialEditComponent } from './material/material-edit/material-edit.component';
+import { UIRouterModule } from '@uirouter/angular'
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+
+
+
+
+let toolState = { name: 'tool', url: 'tools/tool-list', component: ToolListComponent };
+let materialState = { name: 'material', url: 'material/material-list', component: MaterialListComponent };
 
 @NgModule({
   declarations: [
@@ -16,9 +23,12 @@ import { MaterialEditComponent } from './material/material-edit/material-edit.co
     MaterialEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UIRouterModule.forRoot({ states: [toolState, materialState], useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+platformBrowserDynamic.apply(AppModule);
