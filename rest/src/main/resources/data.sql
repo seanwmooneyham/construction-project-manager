889753575acb90f1fc985c15ba46b1c2ec7e65f2
Sean Mooneyham <mooneyhams@pragmatics.com>
2018-09-13 17:29:11 -0400

    add DDL and DML scripts to initialize database and provide seed data

--- rest/src/main/resources/data.sql
+++ rest/src/main/resources/data.sql
@@ -1,15 +1,3 @@
-CREATE TABLE IF NOT EXISTS tool(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(75)
-);
-
-
-CREATE TABLE IF NOT EXISTS material(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(75)
-);
-
-
 INSERT INTO tool VALUES(null, 'Hammer Drill');
 INSERT INTO tool VALUES(null, 'Battery Drill');
 INSERT INTO tool VALUES(null, 'Pipe Bender');
@@ -21,6 +9,3 @@ INSERT INTO material VALUES(null, '1/4" EMT Couplings');
 INSERT INTO material VALUES(null, '1/4 - 20 Screws');
 INSERT INTO material VALUES(null, '1/2" Rigid');
 INSERT INTO material VALUES(null, '4" Square Electrical Box');
-
-
-
